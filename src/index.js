1cff5817a0ef0caf5cac2fe5ba1784d14b50f232
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-09-27 15:14:14 +0330

    Building Redux from Scratch

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 // import "./Functional/index";
-import store from "./store";
+// import store from "./store";
 import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
-let unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
+// let unsubscribe = store.subscribe(() => {
+//   console.log(store.getState());
+// });
+
+// store.dispatch(bugAdded("bug1"));
+// store.dispatch(bugAdded("bug2"));
+// store.dispatch(bugResolved(2));
+
+// unsubscribe();
+
+/* ------------------------------ custom store ------------------------------ */
+import store from "./customStore";
+
+store.subscribe(() => {
+  console.log("State Changed!", store.getState());
 });
 
 store.dispatch(bugAdded("bug1"));
 store.dispatch(bugAdded("bug2"));
-store.dispatch(bugResolved(2));
+store.dispatch(bugAdded("bug3"));
 
-unsubscribe();
+console.log(store.getState());
+/* -------------------------------------------------------------------------- */
