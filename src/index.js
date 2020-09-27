694e8d3412539973b02c444fb453663d19f4bca2
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-09-27 15:33:20 +0330

    Installing Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 // import "./Functional/index";
-// import store from "./store";
+import store from "./store";
 import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
-// let unsubscribe = store.subscribe(() => {
-//   console.log(store.getState());
-// });
-
-// store.dispatch(bugAdded("bug1"));
-// store.dispatch(bugAdded("bug2"));
-// store.dispatch(bugResolved(2));
-
-// unsubscribe();
-
-/* ------------------------------ custom store ------------------------------ */
-import store from "./customStore";
-
-store.subscribe(() => {
-  console.log("State Changed!", store.getState());
+let unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
 });
 
 store.dispatch(bugAdded("bug1"));
 store.dispatch(bugAdded("bug2"));
-store.dispatch(bugAdded("bug3"));
+store.dispatch(bugResolved(2));
 
-console.log(store.getState());
+unsubscribe();
+
+/* ------------------------------ custom store ------------------------------ */
+// import store from "./customStore";
+
+// store.subscribe(() => {
+//   console.log("State Changed!", store.getState());
+// });
+
+// store.dispatch(bugAdded("bug1"));
+// store.dispatch(bugAdded("bug2"));
+// store.dispatch(bugAdded("bug3"));
+
+// console.log(store.getState());
 /* -------------------------------------------------------------------------- */
