93829e0bb10b8ed1d2d932036132280c13907530
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-09-27 11:08:02 +0330

    Dispaching & Subscribing to the Store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 // import "./Functional/index";
+import store from "./store";
+
+let unsubscribe = store.subscribe(() => {
+  console.log("hi", store.getState());
+});
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "bug1",
+  },
+});
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "bug2",
+  },
+});
+
+console.log(store.getState());
+
+unsubscribe();
