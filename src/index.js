15cdbc2523bb4165d5fc9fd89e87cdff3538917b
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-09-27 11:12:09 +0330

    Action Types

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 // import "./Functional/index";
 import store from "./store";
+import { BUG_ADDED, BUG_REMOVED } from "./actionTypes";
 
 let unsubscribe = store.subscribe(() => {
   console.log("hi", store.getState());
 });
 
 store.dispatch({
-  type: "bugAdded",
+  type: BUG_ADDED,
   payload: {
     description: "bug1",
   },
 });
 store.dispatch({
-  type: "bugAdded",
+  type: BUG_ADDED,
   payload: {
     description: "bug2",
+  },
+});
+store.dispatch({
+  type: BUG_REMOVED,
+  payload: {
+    id: 2,
   },
 });
 
