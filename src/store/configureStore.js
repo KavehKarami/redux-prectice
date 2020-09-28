5360613ca5d7794c6a80a17ec917aed120366da9
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-09-28 22:09:47 +0330

    write a simple middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,12 @@
 // import { createStore } from "redux";
 // import { devToolsEnhancer } from "redux-devtools-extension";
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "./middleware/logger";
 import reducer from "./reducer";
 
 export default function () {
-  return configureStore({ reducer });
+  return configureStore({
+    reducer,
+    /* , middleware: [logger] */
+  });
 }
---
+++ src/store/middleware/logger.js
@@ -0,0 +1,12 @@
+// NOTE middleware is the piece of code that get exected "after an action is dispatched" and "before is reached the root reducer"
+// NOTE store is a same with store but it only have a getState and dispatch method
+// NOTE next is works like next in node.js; if this is one middleware that we have, next is a root reducer; but if we have a multiple middleware, next is a NEXT middleware
+// NOTE action is your redux action
+
+const logger = (store) => (next) => (action) => {
+  console.log("store: ", store);
+  console.log("next: ", next);
+  console.log("action: ", action);
+};
+
+export default logger;
