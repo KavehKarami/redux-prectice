bed08531068a9cbe27fb81dac3f541b55ba9440c
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-09-28 20:13:59 +0330

    **** Memoizing Selectors with Reselect ****

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
 
 let lastId = 0;
 const slice = createSlice({
@@ -28,9 +29,14 @@ const slice = createSlice({
 export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
 export default slice.reducer;
 
-export const getUnresolvedBugs = (state) => {
-  return state.entities.bugs.filter((bug) => !bug.resolved);
-};
+// export const getUnresolvedBugs = (state) => {
+//   return state.entities.bugs.filter((bug) => !bug.resolved);
+// };
+
+export const getUnresolvedBugs = createSelector(
+  (state) => state.entities.bugs,
+  (bugs) => bugs.filter((bug) => !bug.resolved)
+);
 
 /* ------------------------ a way with redux toolkit ------------------------ */
 
