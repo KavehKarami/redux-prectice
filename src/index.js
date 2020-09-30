5dfb035190b5a369a851e96c5ff7c438a5105b20
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-09-30 12:08:51 +0330

    clean index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-// import "./Functional/index";
 import configureStore from "./store/configureStore";
-// import {
-//   bugAdded,
-//   bugRemoved,
-//   bugResolved,
-//   getUnresolvedBugs,
-//   bugAssignedToUser,
-//   getBugsByUser,
-// } from "./store/bugs";
-// import { projectAdded, projectRemoved } from "./store/projects";
-// import { userAdded } from "./store/users";
 
 const store = configureStore();
 
@@ -17,48 +6,4 @@ let unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-// using thunk middleware to handle dispatching a function
-store.dispatch((dispatch, getState) => {
-  dispatch({ type: "bugsRecived", bugs: [1, 2, 3, 4] });
-});
-
-store.dispatch({
-  type: "error",
-  payload: { message: "there is some error in your code" },
-});
-
-// store.dispatch(userAdded({ name: "Kaveh" }));
-// store.dispatch(userAdded({ name: "Kasra" }));
-
-// store.dispatch(projectAdded({ name: "Kaveh" }));
-// store.dispatch(projectAdded({ name: "Kasra" }));
-// store.dispatch(projectAdded({ name: "alaki" }));
-// store.dispatch(projectRemoved({ id: 3 }));
-
-// store.dispatch(bugAdded({ description: "bug1" }));
-// store.dispatch(bugAdded({ description: "bug2" }));
-// store.dispatch(bugResolved({ id: 2 }));
-
-// store.dispatch(bugAssignedToUser({ userId: 1, bugId: 1 }));
-
-// const unresolvedBugs = getUnresolvedBugs(store.getState());
-// console.log(unresolvedBugs);
-
-// const userAssignedBugs = getBugsByUser(1)(store.getState());
-// console.log(userAssignedBugs);
-
 unsubscribe();
-
-/* ------------------------------ custom store ------------------------------ */
-// import store from "./customStore";
-
-// store.subscribe(() => {
-//   console.log("State Changed!", store.getState());
-// });
-
-// store.dispatch(bugAdded("bug1"));
-// store.dispatch(bugAdded("bug2"));
-// store.dispatch(bugAdded("bug3"));
-
-// console.log(store.getState());
-/* -------------------------------------------------------------------------- */
