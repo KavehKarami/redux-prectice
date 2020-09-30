f40c149b4ae386d71432b22adbd1d4544fe9a6ba
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-09-30 16:46:46 +0330

    calling a Api and create a action and show api respinse in payload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import configureStore from "./store/configureStore";
+import { apiCallBegan } from "./store/api";
 
 const store = configureStore();
 
 let unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
+
+store.dispatch(apiCallBegan({ url: "/bugs-api" }));
+
+console.log(store.getState());
 
 unsubscribe();
---
+++ src/store/middleware/api.js
@@ -0,0 +1,29 @@
+import Axios from "axios";
+import { apiCallBegan, apiCallSuccess, apiCallFailed } from "./../api";
+
+const api = ({ dispatch }) => (next) => async (action) => {
+  if (action.type !== apiCallBegan.type) return next(action);
+
+  next(action);
+  const { url, method, data, onSuccess, onError } = action.payload;
+
+  try {
+    const response = await Axios({
+      baseURL: "http://localhost:8000/api",
+      url,
+      method,
+      data,
+    });
+    //general
+    dispatch(apiCallSuccess(response.data));
+    // specific
+    if (onSuccess) dispatch({ type: onSuccess, payload: response.data });
+  } catch (err) {
+    //general
+    dispatch(apiCallFailed(err.message));
+    // specific
+    if (onError) dispatch({ type: onError, payload: err.message });
+  }
+};
+
+export default api;
