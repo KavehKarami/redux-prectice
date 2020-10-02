9f8725cf82f780648afda9faabf8014f785cd029
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-10-02 11:54:06 +0330

    Mocking HTTP Calls

--- src/store/test/bugs.spec.js
+++ src/store/test/bugs.spec.js
@@ -1,11 +1,19 @@
+import MockAdapter from "axios-mock-adapter";
+import Axios from "axios";
 import { addBug } from "../bugs";
 import configureStore from "./../configureStore";
 
 describe("bugsSlice", () => {
   it("should handle the addBug action", async () => {
-    const store = configureStore();
     const bug = { description: "a" };
+    const savedBug = { ...bug, _id: 1 };
+
+    const fakeAxios = new MockAdapter(Axios);
+    fakeAxios.onPost("/bugs").reply(200, savedBug);
+
+    const store = configureStore();
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
